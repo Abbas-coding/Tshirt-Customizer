6eb1c7555d4cfa1867f8c197181cbd43f14d4767
Abbas-coding <abbasfakhruddin205@gmail.com>
2023-10-23 18:31:30 +0500

    Tabs and shirt

---
+++ client/src/canvas/Backdrop.jsx
@@ -0,0 +1,18 @@
+import React, {useRef} from 'react';
+import { easing } from 'maath';
+import { useFrame } from '@react-three/fiber';
+import {AccumulativeShadows, RandomizedLight} from '@react-three/drei';
+
+
+const Backdrop = () => {
+  return (
+    <AccumulativeShadows
+    position={[0,0,0.14]}>
+      <RandomizedLight
+      amount={4} />
+      <RandomizedLight />
+    </AccumulativeShadows>
+  )
+}
+
+export default Backdrop
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -26,10 +26,28 @@ const Customizer = () => {
                   key={tab.name}
                   tab={tab}
                   handleClick={() => {}} >
-                    Hrrlo</Tab>
+                    </Tab>
                 ))}
               </div>
             </div>
+          </motion.div>
+          <motion.div className='absolute z-10 top-5 right-5' {...fadeAnimation}>
+            <CustomButton type="filled"
+            title="Go Back"
+            handleClick={()=> state.intro = true}
+            customStyles="w-fit px-4 py-2.5 font-bold text-sm" />
+          </motion.div>
+
+          <motion.div className='filtertabs-container' {...slideAnimation('up')}>
+            {FilterTabs.map((tab) => (
+              <Tab 
+              key={tab.name}
+              tab={tab}
+              isFilterTab
+              isActiveTab=""
+              handleClick={() => {}} >
+              </Tab>
+            ))}
           </motion.div>
         </>
       )}
